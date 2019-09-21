cbaf154bc131d5363816cf95565db1bf769b91ab
Dhruv <dmehta32@gatech.edu>
2019-09-22 02:00:42 +0400

    Added Termination conditions for recursion and updated addEvidence method

--- ML4T_2019Fall/assess_learners/DTLearner.py
+++ ML4T_2019Fall/assess_learners/DTLearner.py
@@ -4,12 +4,25 @@ class DTLearner(object):
 
     def __init__(self, leaf_size=1, verbose=False):
         self.leaf_size = leaf_size
+        self.verbose = verbose
+        self.model = []
 
     def author(self):
         return 'dmehta32'
 
     def addEvidence(self, dataX, dataY):
-        pass
+        self.model = self.build_tree(dataX, dataY)
+
+    def build_tree(self, dataX, dataY):
+        if dataX.shape[0] <= self.leaf_size:
+            return np.asarray([["Leaf", np.mean(dataY), np.nan, np.nan]])
+
+        elif np.unique(dataY).shape[0] == 1:
+            return np.asarray([["Leaf", dataY[0], np.nan, np.nan]])
+
+        else:
+            split_val = self.select_splitval(dataX, dataY)
+            return None
 
     def query(self, points):
         pass
