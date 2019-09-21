28f95f417b13b65b10317c7b558ccf8010191103
Dhruv <dmehta32@gatech.edu>
2019-09-22 01:29:05 +0400

    First commit for DTLearner

---
+++ ML4T_2019Fall/assess_learners/DTLearner.py
@@ -0,0 +1,19 @@
+import numpy as np
+
+class DTLearner(object):
+
+    def __init__(self, leaf_size=1, verbose=False):
+        self.leaf_size = leaf_size
+
+    def author(self):
+        return 'dmehta32'
+
+    def addEvidence(self, dataX, dataY):
+        pass
+
+    def query(self, points):
+        pass
+
+
+if __name__=="__main__":
+    print("the secret clue is 'zzyzx'")
