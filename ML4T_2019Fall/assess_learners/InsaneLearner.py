8df1f4957f1376e507fb7c1c8b87b9d496ce1106
Dhruv <dmehta32@gatech.edu>
2019-09-22 08:28:47 +0400

    Implemented InsaneLearner

---
+++ ML4T_2019Fall/assess_learners/InsaneLearner.py
@@ -0,0 +1,16 @@
+from assess_learners import BagLearner as bg
+from assess_learners import LinRegLearner as lrl
+
+
+class InsaneLearner(object):
+    def __init__(self, verbose=False):
+        self.learner = bg.BagLearner(learner=lrl.LinRegLearner, kwargs={}, bags=20)
+
+    def author(self):
+        return 'dmehta32'
+
+    def addEvidence(self, dataX, dataY):
+        self.learner.addEvidence(dataX, dataY)
+
+    def query(self, points):
+        return self.learner.query(points)
