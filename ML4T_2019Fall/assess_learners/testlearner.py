ad74425752d3ce155b3dd0982384af237e88547d
Dhruv <dmehta32@gatech.edu>
2019-09-23 05:33:05 +0400

    Added code to generate plots for Experiment 1

--- ML4T_2019Fall/assess_learners/testlearner.py
+++ ML4T_2019Fall/assess_learners/testlearner.py
@@ -25,6 +25,8 @@ GT honor code violation.
 import numpy as np  		   	  			  	 		  		  		    	 		 		   		 		  
 import math
 import sys
+import pandas as pd
+import matplotlib.pyplot as plt
 import LinRegLearner as lrl
 import DTLearner as dt
 import RTLearner as rt
@@ -34,9 +36,50 @@ import BagLearner as bg
 def gtid():
     return 902831571  # replace with your GT ID number
 
+def experiment1_figure1():
+    inf = open("Data/Istanbul.csv")
+    data_exp1 = np.genfromtxt(inf, delimiter=",")
+    data_exp1 = data_exp1[1:, 1:]
+
+    # compute how much of the data is training and testing
+    train_rows_exp1 = int(0.6 * data_exp1.shape[0])
+    test_rows_exp1 = data_exp1.shape[0] - train_rows_exp1
+
+    # separate out training and testing data
+    trainX_exp1 = data_exp1[:train_rows_exp1, 0:-1]
+    trainY_exp1 = data_exp1[:train_rows_exp1, -1]
+    testX_exp1 = data_exp1[train_rows_exp1:, 0:-1]
+    testY_exp1 = data_exp1[train_rows_exp1:, -1]
+
+    # create list of rmse
+    in_sample_rmse = []
+    out_sample_rmse = []
+    leaf_index = np.arange(1, 51)
+    for leaf_size in leaf_index:
+        learner_exp1 = dt.DTLearner(leaf_size=leaf_size)
+        learner_exp1.addEvidence(trainX_exp1, trainY_exp1)
+        predY_exp1 = learner_exp1.query(trainX_exp1)  # get the predictions
+        insample_rmse = math.sqrt(((trainY_exp1 - predY_exp1) ** 2).sum() / trainY_exp1.shape[0])
+        in_sample_rmse.append(insample_rmse)
+        predY_exp1 = learner_exp1.query(testX_exp1)
+        outsample_rmse = math.sqrt(((testY_exp1 - predY_exp1) ** 2).sum() / testY_exp1.shape[0])
+        out_sample_rmse.append(outsample_rmse)
+
+    fig, axis = plt.subplots(figsize=(12, 8))
+    df = pd.DataFrame({"In-Sample RMSE": in_sample_rmse, "Out-Sample RMSE": out_sample_rmse}, index=leaf_index)
+    df.plot(ax=axis, title="RMSE vs Leaf Size for DTLearner")
+    axis.set_xlabel("Leaf Size")
+    axis.set_ylabel("RMSE")
+    plt.axvline(x=10, color='gray', linestyle='--')
+    plt.axvline(x=19, color='gray', linestyle='--')
+    plt.legend(["In-Sample RMSE", "Out-Sample RMSE", "Leaf-Size=10", "Leaf-Size=19"], loc='lower right')
+    plt.tight_layout()
+    plt.savefig("exp1-fig1.png")
+
 
 if __name__ == "__main__":
 
+    experiment1_figure1()
     # Credits - Piazza Post # 578
     if len(sys.argv) != 2:
         print("Usage: python testlearner.py <filename>")
@@ -65,9 +108,9 @@ if __name__ == "__main__":
 
     # create a learner and train it  		   	  			  	 		  		  		    	 		 		   		 		  
     # learner = lrl.LinRegLearner(verbose = True) # create a LinRegLearner
-    # learner = dt.DTLearner(leaf_size=1, verbose=False)
+    learner = dt.DTLearner(leaf_size=1, verbose=False)
     # learner = rt.RTLearner(leaf_size=1, verbose=False)
-    learner = bg.BagLearner(learner=dt.DTLearner, kwargs={"leaf_size":1}, bags=20)
+    # learner = bg.BagLearner(learner=dt.DTLearner, kwargs={"leaf_size":1}, bags=20)
     learner.addEvidence(trainX, trainY)  # train it
     print(learner.author())  		   	  			  	 		  		  		    	 		 		   		 		  
 
@@ -87,4 +130,5 @@ if __name__ == "__main__":
     print("Out of sample results")  		   	  			  	 		  		  		    	 		 		   		 		  
     print(f"RMSE: {rmse}")  		   	  			  	 		  		  		    	 		 		   		 		  
     c = np.corrcoef(predY, y=testY)  		   	  			  	 		  		  		    	 		 		   		 		  
-    print(f"corr: {c[0,1]}")  		   	  			  	 		  		  		    	 		 		   		 		  
+    print(f"corr: {c[0,1]}")
+
