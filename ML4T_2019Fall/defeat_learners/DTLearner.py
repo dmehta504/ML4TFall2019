a62edb4736dbe077cbf49c6356b4ec2ee1ca6ef9
Dhruv <dmehta32@gatech.edu>
2019-09-26 14:27:40 +0400

    Replacing DTLearner with working version from assess_learners

--- ML4T_2019Fall/defeat_learners/DTLearner.py
+++ ML4T_2019Fall/defeat_learners/DTLearner.py
@@ -1,64 +1,95 @@
-"""  		   	  			  	 		  		  		    	 		 		   		 		  
-A simple wrapper for linear regression.  (c) 2015 Tucker Balch  		   	  			  	 		  		  		    	 		 		   		 		  
-Note, this is NOT a correct DTLearner; Replace with your own implementation.  		   	  			  	 		  		  		    	 		 		   		 		  
-Copyright 2018, Georgia Institute of Technology (Georgia Tech)  		   	  			  	 		  		  		    	 		 		   		 		  
-Atlanta, Georgia 30332  		   	  			  	 		  		  		    	 		 		   		 		  
-All Rights Reserved  		   	  			  	 		  		  		    	 		 		   		 		  
-  		   	  			  	 		  		  		    	 		 		   		 		  
-Template code for CS 4646/7646  		   	  			  	 		  		  		    	 		 		   		 		  
-  		   	  			  	 		  		  		    	 		 		   		 		  
-Georgia Tech asserts copyright ownership of this template and all derivative  		   	  			  	 		  		  		    	 		 		   		 		  
-works, including solutions to the projects assigned in this course. Students  		   	  			  	 		  		  		    	 		 		   		 		  
-and other users of this template code are advised not to share it with others  		   	  			  	 		  		  		    	 		 		   		 		  
-or to make it available on publicly viewable websites including repositories  		   	  			  	 		  		  		    	 		 		   		 		  
-such as github and gitlab.  This copyright statement should not be removed  		   	  			  	 		  		  		    	 		 		   		 		  
-or edited.  		   	  			  	 		  		  		    	 		 		   		 		  
-  		   	  			  	 		  		  		    	 		 		   		 		  
-We do grant permission to share solutions privately with non-students such  		   	  			  	 		  		  		    	 		 		   		 		  
-as potential employers. However, sharing with other current or future  		   	  			  	 		  		  		    	 		 		   		 		  
-students of CS 7646 is prohibited and subject to being investigated as a  		   	  			  	 		  		  		    	 		 		   		 		  
-GT honor code violation.  		   	  			  	 		  		  		    	 		 		   		 		  
-  		   	  			  	 		  		  		    	 		 		   		 		  
------do not edit anything above this line---  		   	  			  	 		  		  		    	 		 		   		 		  
-  		   	  			  	 		  		  		    	 		 		   		 		  
-Student Name: Tucker Balch (replace with your name)  		   	  			  	 		  		  		    	 		 		   		 		  
-GT User ID: tb34 (replace with your User ID)  		   	  			  	 		  		  		    	 		 		   		 		  
-GT ID: 900897987 (replace with your GT ID)  		   	  			  	 		  		  		    	 		 		   		 		  
-"""  		   	  			  	 		  		  		    	 		 		   		 		  
-  		   	  			  	 		  		  		    	 		 		   		 		  
-import numpy as np  		   	  			  	 		  		  		    	 		 		   		 		  
-import warnings  		   	  			  	 		  		  		    	 		 		   		 		  
-  		   	  			  	 		  		  		    	 		 		   		 		  
-class DTLearner(object):  		   	  			  	 		  		  		    	 		 		   		 		  
-  		   	  			  	 		  		  		    	 		 		   		 		  
-    def __init__(self, leaf_size=1, verbose = False):  		   	  			  	 		  		  		    	 		 		   		 		  
-        warnings.warn("\n\n  WARNING! THIS IS NOT A CORRECT DTLearner IMPLEMENTATION! REPLACE WITH YOUR OWN CODE\n")  		   	  			  	 		  		  		    	 		 		   		 		  
-        pass # move along, these aren't the drones you're looking for  		   	  			  	 		  		  		    	 		 		   		 		  
-  		   	  			  	 		  		  		    	 		 		   		 		  
-    def author(self):  		   	  			  	 		  		  		    	 		 		   		 		  
-        return 'tb34' # replace tb34 with your Georgia Tech username  		   	  			  	 		  		  		    	 		 		   		 		  
-  		   	  			  	 		  		  		    	 		 		   		 		  
-    def addEvidence(self,dataX,dataY):  		   	  			  	 		  		  		    	 		 		   		 		  
-        """  		   	  			  	 		  		  		    	 		 		   		 		  
-        @summary: Add training data to learner  		   	  			  	 		  		  		    	 		 		   		 		  
-        @param dataX: X values of data to add  		   	  			  	 		  		  		    	 		 		   		 		  
-        @param dataY: the Y training values  		   	  			  	 		  		  		    	 		 		   		 		  
-        """  		   	  			  	 		  		  		    	 		 		   		 		  
-  		   	  			  	 		  		  		    	 		 		   		 		  
-        # slap on 1s column so linear regression finds a constant term  		   	  			  	 		  		  		    	 		 		   		 		  
-        newdataX = np.ones([dataX.shape[0],dataX.shape[1]+1])  		   	  			  	 		  		  		    	 		 		   		 		  
-        newdataX[:,0:dataX.shape[1]]=dataX  		   	  			  	 		  		  		    	 		 		   		 		  
-  		   	  			  	 		  		  		    	 		 		   		 		  
-        # build and save the model  		   	  			  	 		  		  		    	 		 		   		 		  
-        self.model_coefs, residuals, rank, s = np.linalg.lstsq(newdataX, dataY, rcond=None)  		   	  			  	 		  		  		    	 		 		   		 		  
-  		   	  			  	 		  		  		    	 		 		   		 		  
-    def query(self,points):  		   	  			  	 		  		  		    	 		 		   		 		  
-        """  		   	  			  	 		  		  		    	 		 		   		 		  
-        @summary: Estimate a set of test points given the model we built.  		   	  			  	 		  		  		    	 		 		   		 		  
-        @param points: should be a numpy array with each row corresponding to a specific query.  		   	  			  	 		  		  		    	 		 		   		 		  
-        @returns the estimated values according to the saved model.  		   	  			  	 		  		  		    	 		 		   		 		  
-        """  		   	  			  	 		  		  		    	 		 		   		 		  
-        return (self.model_coefs[:-1] * points).sum(axis = 1) + self.model_coefs[-1]  		   	  			  	 		  		  		    	 		 		   		 		  
-  		   	  			  	 		  		  		    	 		 		   		 		  
-if __name__=="__main__":  		   	  			  	 		  		  		    	 		 		   		 		  
-    print("the secret clue is 'zzyzx'")  		   	  			  	 		  		  		    	 		 		   		 		  
+import numpy as np
+
+
+class DTLearner(object):
+
+    def __init__(self, leaf_size=1, verbose=False):
+        self.leaf_size = leaf_size
+        self.verbose = verbose
+        self.model = []
+
+    def author(self):
+        return 'dmehta32'
+
+    def addEvidence(self, dataX, dataY):
+        self.model = self.build_tree(dataX, dataY)
+
+    def build_tree(self, dataX, dataY):
+        # Check for termination criteria
+
+        # We have only one sample left (when size = 1), or our sample size is less than the leaf size
+        if dataX.shape[0] <= self.leaf_size:
+            return np.asarray([["Leaf", np.mean(dataY), np.nan, np.nan]])
+
+        # If all values in dataY are the same, then all values of X will result in the same Y value
+        if np.unique(dataY).shape[0] == 1:
+            return np.asarray([["Leaf", dataY[0], np.nan, np.nan]])
+
+        else:
+            # Find the best factor to split the nodes on
+            best_factor, best_index = self.select_splitval(dataX, dataY)
+            split_val = np.median(dataX[:, best_index])
+            max_feature = max(dataX[:, best_index])
+
+            # Extra termination check - to avoid infinite recursion
+            # Credits : Piazza post - #385
+            if max_feature == split_val:
+                return np.asarray([["Leaf", np.mean(dataY), np.nan, np.nan]])
+
+            # Recursively build the left & right trees
+            left_tree = self.build_tree(dataX[dataX[:, best_index] <= split_val],
+                                        dataY[dataX[:, best_index] <= split_val])
+            right_tree = self.build_tree(dataX[dataX[:, best_index] > split_val],
+                                         dataY[dataX[:, best_index] > split_val])
+            root = np.asarray([[best_index, split_val, 1, left_tree.shape[0] + 1]], dtype=float)
+            return np.vstack((root, left_tree, right_tree))
+
+    def select_splitval(self, factors, results):
+        """
+        @summary: Select the best feature used to split the values in decision tree
+        @param factors: Factors used in selection of best factor
+        @param results: the Y values or results
+        """
+        best_factor = 0
+        best_index = 0
+        for i in range(factors.shape[1]):
+            correlation = np.corrcoef(factors[:, i], results)[0, 1]
+            if abs(correlation) > best_factor:
+                best_factor = abs(correlation)
+                best_index = i
+
+        return best_factor, best_index
+
+    def query(self, points):
+        results = []
+        for i in range(points.shape[0]):
+            result = self.get_y_val(points[i, :])
+            results.append(float(result))
+
+        return np.asarray(results)
+
+    def get_y_val(self, x_val):
+        """
+        @summary: Returns the Y value/result based on the decision tree model learned
+        @param x_val: The specific query used to find the Y value/result
+        """
+        i = 0
+
+        # Iterate through the decision tree until we reach the desired leaf
+        while self.model[i][0] != "Leaf":
+            split_val = self.model[i][1]
+
+            # If value of query is less than the split_val, go in the left tree
+            if x_val[int(float(self.model[i][0]))] <= float(split_val):
+                # Convert to float then int to avoid runtime errors
+                i = i + int(float(self.model[i][2]))
+            # Else go in the right tree
+            else:
+                i = i + int(float(self.model[i][3]))  # This gets the next node we have to search in the right tree
+
+        # If leaf reached, then return the Y-value from our model
+        return self.model[i][1]
+
+
+if __name__=="__main__":
+    print("the secret clue is 'zzyzx'")
