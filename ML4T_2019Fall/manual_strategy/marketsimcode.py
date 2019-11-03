1a80b7fa7d949ebd555a99f414fa2518b264ffbd
Dhruv <dmehta32@gatech.edu>
2019-11-03 20:18:13 +0400

    First commit - creating marketsimcode.py file

---
+++ ML4T_2019Fall/manual_strategy/marketsimcode.py
@@ -0,0 +1,9 @@
+""" Fall 2019 - Project 6 : Manual Strategy
+Student Name: Dhruv Mehta (replace with your name)
+GT User ID: dmehta32 (replace with your User ID)
+GT ID: 902831571 (replace with your GT ID)
+"""
+
+
+def author():
+    return 'dmehta32'
