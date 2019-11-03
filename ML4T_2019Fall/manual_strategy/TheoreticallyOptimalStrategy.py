a3b38079010c49b8f14ef842bc9fc4658541cd88
Dhruv <dmehta32@gatech.edu>
2019-11-04 03:30:41 +0400

    Fixed some bugs in the code

--- ML4T_2019Fall/manual_strategy/TheoreticallyOptimalStrategy.py
+++ ML4T_2019Fall/manual_strategy/TheoreticallyOptimalStrategy.py
@@ -14,13 +14,13 @@ import marketsimcode as ms
 def testPolicy(symbol="JPM", sd=dt.datetime(2008, 1, 1), ed=dt.datetime(2009, 12, 31), sv=100000):
     # Get the prices for SPY and JPM
     dates = pd.date_range(sd, ed)
-    prices_all = util.get_data(symbol, dates)
+    prices_all = util.get_data([symbol], dates)
     prices_all = prices_all / prices_all.iloc[0]  # Normalize the prices
     prices_SPY = prices_all['SPY']
     prices_JPM = prices_all['JPM']
     daily_returns = compute_daily_returns(prices_JPM)
 
-    df_trades = pd.DataFrame(index=prices_SPY.index, columns=symbol)
+    df_trades = pd.DataFrame(index=prices_SPY.index, columns=[symbol])
     current_holding = 0
     date_last = None
     for date in prices_SPY.index:
@@ -65,7 +65,13 @@ def compute_daily_returns(prices):
 
 def test_code():
     df_trades = testPolicy()
-    portvals = ms.compute_portvals(df_trades, start_val=1000000, commission=0, impact=0)
+    portvals = ms.compute_portvals(df_trades, start_val=100000, commission=0, impact=0)
+    if isinstance(portvals, pd.DataFrame):
+        portvals = portvals[
+            portvals.columns[0]]  # just get the first column
+    else:
+        "warning, code did not return a DataFrame"
+
     cumulative_return, avg_daily_ret, std_daily_ret, sharpe_ratio = ms.compute_portfolio_stats(portvals)
     print(cumulative_return, avg_daily_ret, std_daily_ret, sharpe_ratio)
 
