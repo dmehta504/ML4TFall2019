efb1658a70b279f17bc0f638edf4ee1eb78fed2b
Dhruv <dmehta32@gatech.edu>
2019-11-04 02:19:54 +0400

    First commit - Creating TheoreticallyOptimalStrategy.py file

---
+++ ML4T_2019Fall/manual_strategy/TheoreticallyOptimalStrategy.py
@@ -0,0 +1,14 @@
+""" Fall 2019 - Project 6 : Manual Strategy
+Student Name: Dhruv Mehta (replace with your name)
+GT User ID: dmehta32 (replace with your User ID)
+GT ID: 902831571 (replace with your GT ID)
+"""
+
+import util
+import numpy as np
+import pandas as pd
+import datetime as dt
+
+
+def testPolicy(symbol="JPM", sd=dt.datetime(2008, 1, 1), ed=dt.datetime(2009, 12, 31), sv=100000):
+    pass
