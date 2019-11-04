d8ecccdb42884817ead9cd24e962d3d424cd804a
Dhruv <dmehta32@gatech.edu>
2019-11-04 07:19:28 +0400

    Modified code to reduce number of lines

--- ML4T_2019Fall/manual_strategy/marketsimcode.py
+++ ML4T_2019Fall/manual_strategy/marketsimcode.py
@@ -30,46 +30,27 @@ def compute_portfolio_stats(portvals):
     return cumulative_return, avg_daily_ret, std_daily_ret, sharpe_ratio
 
 
-def compute_portvals(df_trades, start_val=1000000, commission=9.95, impact=0.005):
-    # this is the function the autograder will call to test your code
-    # NOTE: orders_file may be a string, or it may be a file object. Your
-    # code should work correctly with either input
-
-    # Get the orders from file
-    orders = df_trades
-    start_date = orders.index.min()
-    end_date = orders.index.max()
-
-    # In the template, instead of computing the value of the portfolio, we just
-    # read in the value of IBM over 6 months
-    # start_date = dt.datetime(2008, 1, 1)
-    # end_date = dt.datetime(2008, 6, 1)
-    portvals_SPY = util.get_data(['SPY'], pd.date_range(start_date, end_date))
-    portvals_SPY = portvals_SPY[['SPY']]  # remove SPY
-    dates_index = pd.date_range(start_date, end_date, freq='D')
-
-    # Remove the dates SPY didn't trade on
-    for date in dates_index:
-        if date not in portvals_SPY.index:
-            dates_index = dates_index.drop(date)
-
-    # Get the prices of the stocks that were traded in the orders file, forward fill then back-fill for missing values
-    symbols = df_trades.columns.unique().tolist()
-    symbol_dict = {}
+def get_stock_prices(symbols, dates):
+    df_prices = pd.DataFrame(index=dates, columns=symbols)
     for symbol in symbols:
-        symbol_dict[symbol] = util.get_data([symbol], pd.date_range(start_date, end_date), colname='Adj Close')
-        symbol_dict[symbol] = symbol_dict[symbol].resample("D").fillna(method='ffill')
-        symbol_dict[symbol] = symbol_dict[symbol].fillna(method='bfill')
+        df_prices[symbol] = util.get_data([symbol], pd.date_range(dates[0], dates[-1]), colname='Adj Close')[symbol]
+    return df_prices
+
+
+def compute_portvals(df_trades, start_val=1000000, commission=9.95, impact=0.005):
+    # Get the historical prices for each symbol in df_trades
+    symbols = df_trades.columns.unique().tolist()
+    df_prices = get_stock_prices(symbols, df_trades.index)
 
     # Create a portfolio to track total portfolio value and number of shares of each stock
-    portvals = pd.DataFrame(index=dates_index, columns=["total"] + symbols)
+    portvals = pd.DataFrame(index=df_trades.index, columns=["total"] + symbols)
 
     # Initialize portfolio val at start to starting value, as no money has been made
     total_portfolio_val = start_val
     date_last = None
 
     # Execute the orders and simulate the portfolio
-    for date in dates_index:
+    for date in df_trades.index:
 
         if date_last is None:
             # This is when no trades have been made i.e. first day of trading
@@ -80,11 +61,11 @@ def compute_portvals(df_trades, start_val=1000000, commission=9.95, impact=0.005
             portvals.loc[date, "total"] = 0
 
         # If orders are made on that particular date, calculate new values for stocks & portfolio
-        if date in orders.index:
-            orders_made = orders.loc[[date]]
+        if date in df_trades.index:
+            orders_made = df_trades.loc[[date]]
             for stock in orders_made.columns:
                 number_of_shares = abs(orders_made.iloc[0][stock])
-                price_of_stock = symbol_dict[stock].loc[date, stock]
+                price_of_stock = df_prices.loc[date, stock]
 
                 if orders_made.iloc[0][stock] > 0:
                     price_of_stock = (1 + impact) * price_of_stock
@@ -100,7 +81,7 @@ def compute_portvals(df_trades, start_val=1000000, commission=9.95, impact=0.005
         # Update portfolio value for current date
         for symbol in symbols:
             # Multiply the number of shares * price of stock for that trading day and add it to the portfolio value
-            portvals.loc[date, "total"] += (portvals.loc[date, symbol] * symbol_dict[symbol].loc[date, symbol])
+            portvals.loc[date, "total"] += (portvals.loc[date, symbol] * df_prices.loc[date, symbol])
 
         portvals.loc[date, "total"] += total_portfolio_val
         date_last = date
