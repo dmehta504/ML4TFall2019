668ad572e61056c22142a29d1f56f64032460458
Dhruv <dmehta32@gatech.edu>
2019-11-04 04:31:09 +0400

    First commit - Creating ManualStrategy.py file

---
+++ ML4T_2019Fall/manual_strategy/ManualStrategy.py
@@ -0,0 +1,15 @@
+""" Fall 2019 - Project 6 : Manual Strategy
+Student Name: Dhruv Mehta (replace with your name)
+GT User ID: dmehta32 (replace with your User ID)
+GT ID: 902831571 (replace with your GT ID)
+"""
+
+import util
+import numpy as np
+import pandas as pd
+import datetime as dt
+import marketsimcode as ms
+
+
+def author():
+    return 'dmehta32'
