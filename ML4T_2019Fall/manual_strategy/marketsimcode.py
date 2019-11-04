28c1a0c639324a06d436f22e56c3674b22bf9a3c
Dhruv <dmehta32@gatech.edu>
2019-11-04 09:06:18 +0400

    Minor bug fixes

--- ML4T_2019Fall/manual_strategy/marketsimcode.py
+++ ML4T_2019Fall/manual_strategy/marketsimcode.py
@@ -38,13 +38,17 @@ def get_stock_prices(symbols, dates):
 
 
 def execute_trades(df_trades, df_prices, holdings, start_val, commission, impact):
-    # Create a portfolio to track total portfolio value and number of shares of each stock
+    # Create a portfolio to track total portfolio value
     portvals = pd.DataFrame(index=df_trades.index, columns=["total"])
     total_portfolio_val = start_val
 
     # Execute the orders and simulate the portfolio
     for date in df_trades.index:
         portvals.loc[date, "total"] = 0
+        for symbol in df_prices.columns:
+            # Multiply the number of shares * price of stock for that trading day and add it to the portfolio value
+            portvals.loc[date, "total"] += (holdings.loc[date, symbol] * df_prices.loc[date, symbol])
+
         # If orders are made on that particular date, calculate new values for stocks & portfolio
         if date in df_trades.index:
             orders_made = df_trades.loc[[date]]
@@ -60,11 +64,6 @@ def execute_trades(df_trades, df_prices, holdings, start_val, commission, impact
                     price_of_stock = (1 - impact) * price_of_stock
                     # If shares are sold, add cash amount and subtract commission
                     total_portfolio_val = total_portfolio_val + (price_of_stock * number_of_shares) - commission
-
-        # Update portfolio value for current date
-        for symbol in df_prices.columns:
-            # Multiply the number of shares * price of stock for that trading day and add it to the portfolio value
-            portvals.loc[date, "total"] += (holdings.loc[date, symbol] * df_prices.loc[date, symbol])
 
         portvals.loc[date, "total"] += total_portfolio_val
 
