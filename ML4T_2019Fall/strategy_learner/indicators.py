fc83373f6b042ef81cf72fa1ae7485f04891e16b
Dhruv <dmehta32@gatech.edu>
2019-12-02 05:09:46 +0400

    Added username and GTid

--- ML4T_2019Fall/strategy_learner/indicators.py
+++ ML4T_2019Fall/strategy_learner/indicators.py
@@ -1,4 +1,4 @@
-""" Fall 2019 - Project 6 : Manual Strategy
+""" Fall 2019 - Project 8 : Manual Strategy
 Student Name: Dhruv Mehta (replace with your name)
 GT User ID: dmehta32 (replace with your User ID)
 GT ID: 902831571 (replace with your GT ID)
