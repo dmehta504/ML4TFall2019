933b1c36e30eb2460eca7d1e2cc32dd54bef0feb
Dhruv <dmehta32@gatech.edu>
2019-12-02 05:09:12 +0400

    Added username and GTid

--- ML4T_2019Fall/strategy_learner/RTLearner.py
+++ ML4T_2019Fall/strategy_learner/RTLearner.py
@@ -1,3 +1,9 @@
+""" Fall 2019 - Project 8 : Strategy Learner
+Student Name: Dhruv Mehta (replace with your name)
+GT User ID: dmehta32 (replace with your User ID)
+GT ID: 902831571 (replace with your GT ID)
+"""
+
 import numpy as np
 
 
