f7e6434da45f3d13d1734375c1382e6666efb157
Dhruv <dmehta32@gatech.edu>
2019-12-02 04:39:07 +0400

    First Commit - Creating File for Experiment 2

---
+++ ML4T_2019Fall/strategy_learner/experiment2.py
@@ -0,0 +1,29 @@
+""" Fall 2019 - Project 8 : Strategy Learner
+Student Name: Dhruv Mehta (replace with your name)
+GT User ID: dmehta32 (replace with your User ID)
+GT ID: 902831571 (replace with your GT ID)
+"""
+
+import numpy as np
+import datetime as dt
+import pandas as pd
+import util as ut
+import StrategyLearner as sl
+import BagLearner as bl
+import RTLearner as rt
+import ManualStrategy as mans
+import indicators as ind
+import marketsimcode as ms
+import matplotlib.pyplot as plt
+
+
+def author():
+    return 'dmehta32'
+
+
+def test_code():
+    pass
+
+
+if __name__ == "__main__":
+    test_code()
